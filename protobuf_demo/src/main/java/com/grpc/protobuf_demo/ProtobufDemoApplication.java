1455a5fb556be646bb13262ea836f1cecc189bbc
Nikhil Sharma <sharmanikhilm05@gmail.com>
2025-05-26 11:22:54 +0530

    Initial Commit

---
+++ protobuf_demo/src/main/java/com/grpc/protobuf_demo/ProtobufDemoApplication.java
@@ -0,0 +1,13 @@
+package com.grpc.protobuf_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProtobufDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProtobufDemoApplication.class, args);
+	}
+
+}
---
+++ protobuf_demo/src/test/java/com/grpc/protobuf_demo/ProtobufDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grpc.protobuf_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProtobufDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
