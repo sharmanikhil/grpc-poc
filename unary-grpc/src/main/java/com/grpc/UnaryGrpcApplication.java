049406037b44d80acf84b2c521a8b829851a2d0e
Nikhil Sharma <sharmanikhilm05@gmail.com>
2025-05-28 00:07:20 +0530

    Unary GRPC Set Up

---
+++ unary-grpc/src/main/java/com/grpc/UnaryGrpcApplication.java
@@ -0,0 +1,13 @@
+package com.grpc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnaryGrpcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnaryGrpcApplication.class, args);
+	}
+
+}
